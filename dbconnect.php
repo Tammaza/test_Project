5dc2afebf8f3772c16c08c74f246418f9c755587
Tammaza <tampang-1@hotmail.com>
2022-11-08 19:19:47 +0700

    first commit

---
+++ dbconnect.php
@@ -0,0 +1,18 @@
+<?php
+ 
+ $connect=mysqli_connect("localhost","root","","mydata") or die("cannot connect");
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+ // สิ่งที่ต้องทำคือ เข้า xampp และเข้า phpmyadmin ไปสร้างdatabase 
+?>
